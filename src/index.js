20aeb98b474a6fac65026823240bce29968ec02d
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-14 10:23:15 -0800

    Program fully makes api call with redux and middleware
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import reducer from './reducers/headlines-reducer';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import middlewareLogger from './middleware/middleware-logger';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-reportWebVitals();
